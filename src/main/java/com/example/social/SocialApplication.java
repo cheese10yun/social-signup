bf9504fca07c696b19651d60d0091880742c7fd8
yun-cheese <yun.cheese@kakaocorp.com>
2021-08-23 00:18:57 +0900

    project setting

---
+++ src/main/java/com/example/social/SocialApplication.java
@@ -0,0 +1,12 @@
+package com.example.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplication.class, args);
+    }
+}
