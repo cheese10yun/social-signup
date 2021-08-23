260df2f8471e9994607ab4aa5b5dd4a447760130
yun-cheese <yun.cheese@kakaocorp.com>
2021-08-23 19:35:03 +0900

    project setting

--- src/main/java/com/example/social/config/SecurityConfig.java
+++ src/main/java/com/example/social/config/SecurityConfig.java
@@ -1,14 +1,29 @@
 package com.example.social.config;
 
+import java.util.Arrays;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final Environment environment;
+    private final String registration = "spring.security.oauth2.client.registration.";
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,7 +33,49 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login", "/index").permitAll()
                 .anyRequest().authenticated()
             )
-            .oauth2Login(Customizer.withDefaults())
+            .oauth2Login(oauth2 -> oauth2
+                .clientRegistrationRepository(clientRegistrationRepository())
+                .authorizedClientService(authorizedClientService())
+            )
         ;
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientService authorizedClientService() {
+        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository());
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        final List<ClientRegistration> clientRegistrations = Arrays.asList(
+            googleClientRegistration(),
+            facebookClientRegistration()
+        );
+
+        return new InMemoryClientRegistrationRepository(clientRegistrations);
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        final String clientId = environment.getProperty(registration + "google.client-id");
+        final String clientSecret = environment.getProperty(registration + "google.client-secret");
+
+        return CommonOAuth2Provider
+            .GOOGLE
+            .getBuilder("google")
+            .clientId(clientId)
+            .clientSecret(clientSecret)
+            .build();
+    }
+
+    private ClientRegistration facebookClientRegistration() {
+        final String clientId = environment.getProperty(registration + "facebook.client-id");
+        final String clientSecret = environment.getProperty(registration + "facebook.client-secret");
+
+        return CommonOAuth2Provider
+            .FACEBOOK
+            .getBuilder("facebook")
+            .clientId(clientId)
+            .clientSecret(clientSecret)
+            .build();
     }
 }
